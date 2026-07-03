0d87c292d69285dac87b808878b4293727afa798
Wolf <daniel.wolf@dussmanngroup.com>
2026-07-03 12:23:02 +0200

    Boost für prod entfernt

---
+++ app/Support/ComposerHooks.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Support;
+
+use Composer\Script\Event;
+
+class ComposerHooks
+{
+    public static function boostUpdate(Event $event): void
+    {
+        if (!is_dir(dirname(__DIR__, 2) . '/vendor/laravel/boost')) {
+            return;
+        }
+
+        passthru('php artisan boost:update --ansi');
+    }
+}
