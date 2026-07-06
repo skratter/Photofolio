f97045fe15f5b6cd9583f6a9a386bd792df7a926
skratter <skratter@skratter.com>
2026-07-06 11:48:50 +0200

    Drag&Drop für Upload

--- resources/views/livewire/admin/photo-uploader.blade.php
+++ resources/views/livewire/admin/photo-uploader.blade.php
@@ -1,12 +1,22 @@
 <div
-    x-data="{ isUploading: false, progress: 0 }"
+    x-data="{ isUploading: false, progress: 0, isDraggingOver: false }"
     x-on:livewire-upload-start="isUploading = true"
     x-on:livewire-upload-finish="isUploading = false"
     x-on:livewire-upload-error="isUploading = false"
     x-on:livewire-upload-progress="progress = $event.detail.progress"
 >
-    <label class="flex cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-zinc-300 p-8 text-center hover:border-zinc-400 dark:border-zinc-700 dark:hover:border-zinc-600">
-        <input type="file" wire:model="photos" multiple accept="image/*" class="hidden">
+    <label
+        x-on:dragover.prevent="isDraggingOver = true"
+        x-on:dragleave.prevent="isDraggingOver = false"
+        x-on:drop.prevent="
+            isDraggingOver = false;
+            $refs.input.files = $event.dataTransfer.files;
+            $refs.input.dispatchEvent(new Event('change'));
+        "
+        x-bind:class="isDraggingOver ? 'border-zinc-400 dark:border-zinc-600' : 'border-zinc-300 dark:border-zinc-700'"
+        class="flex cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed p-8 text-center hover:border-zinc-400 dark:hover:border-zinc-600"
+    >
+        <input type="file" x-ref="input" wire:model="photos" multiple accept="image/*" class="hidden">
         <flux:icon name="arrow-up-tray" class="size-8 text-zinc-400" />
         <flux:text class="mt-2">Fotos hierher ziehen oder klicken zum Hochladen</flux:text>
     </label>
